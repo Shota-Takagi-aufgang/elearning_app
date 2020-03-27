0c7f369bb875e97f62b35fbb49c81d6398ef4222
SHOTA TAKAGI <shotatakagi@SHOTAnoMacBook-ea.local>
2020-03-27 16:07:31 +0900

    topic-edit and delete

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,6 @@ Route::get('/category/{category}/topic', function ($category)
 Route::get('/admin/topics/create','TopicController@create')->name('topic.create');
 Route::post('/admin/topics/new', 'TopicController@store')->name('topic.store');
 Route::get('/admin/topics','TopicController@list')->name('topics.list');
-// Route::get('/topics/{id}/edit','TopicController@edit')->name('edit');
-// Route::post('/topics/{id}/update','TopicController@update')->name('update');
-// Route::get('/topics/{id}/delete','TopicController@delete')->name('delete');
+Route::get('/admin/topics/{id}/edit','TopicController@edit')->name('topic.edit');
+Route::post('/admin/topics/{id}/update','TopicController@update')->name('topic.update');
+Route::get('/admin/topics/{id}/delete','TopicController@delete')->name('topic.delete');
